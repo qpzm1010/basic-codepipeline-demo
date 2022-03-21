67692887476a6a46a18a5c6ecf08b4cdfccd8ba0
Lee <euideok@3c06303905c0.ant.amazon.com>
2022-03-21 15:59:39 +0900

    first commit

---
+++ src/main/java/com/example/codepipeline/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.codepipeline;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/sayHello/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name;
+    }
+}
